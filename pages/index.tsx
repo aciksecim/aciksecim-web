efffe9cd428ac3d6c2471e548d3031ceb6130c6c
Muharrem Gönel <90962400+MuharremGonel@users.noreply.github.com>
2023-01-06 18:48:03 +0300

    title1 added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,14 @@ import Nav from '../components/Nav'
 import Header from '../components/Header'
 import CallToAction from '../components/CallToAction'
 import Footer from '../components/Footer'
+import Title1 from '../components/Title1'
+
 const Home: NextPage = () => {
   return (
     <div className="font-aciksecim-font">
       <Nav />
       <Header />
+      <Title1 />
       <CallToAction />
       <Footer />
     </div>
